3bbf9046dada93fa720f43866262d53654a7008c
Muhammad Atif <ma2669637@gmail.com>
2024-03-15 11:53:36 +0500

    added full screen for android and changed add name

--- composeApp/src/androidMain/kotlin/template/network/data/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/template/network/data/MainActivity.kt
@@ -1,17 +1,39 @@
 package template.network.data
 
 import App
+import android.app.Activity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+     /*   WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )*/
 
         setContent {
+            val view = LocalView.current
+            if (!view.isInEditMode) {
+                SideEffect {
+                    val window = (view.context as Activity).window
+                    window.statusBarColor = Color(0xFF202020).toArgb()
+                    window.navigationBarColor = Color(0xFF202020).toArgb()
+
+                }
+            }
+
             App()
         }
     }
